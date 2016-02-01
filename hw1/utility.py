e8950c5281d4fce13b16295da2e5975aa840663f
jinifief <jinified@gmail.com>
2016-02-01 12:47:18 +0800

    Fixes probability calculation

--- hw1/utility.py
+++ hw1/utility.py
@@ -1,13 +1,53 @@
 #!/usr/bin/env python
+import os
 import math
 from itertools import chain
 
 import nltk
-from  nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
 from nltk.probability import *
 
 # Should we lowercase all the words 
+
+class LangModel:
+
+    def __init__(self, lang_name, ngrams, vocab):
+        self.lang_name = lang_name
+        self.vocab = vocab
+        self.ngrams = ngrams
+        self.fdist = FreqDist(ngrams)
+        self.total_smooth = 1
+        self.total_count = self.init_total_samples()
+        self.gram_from_other_LM = math.log(1.0/(self.total_count), 2)
+        self.pdf = self.initProb()
+
+    def init_total_samples(self):
+        self.total_smooth = len(set(self.vocab) - set(self.ngrams))
+        # Calculates total number of samples after smoothing
+        total_count = self.fdist.B()*self.total_smooth + self.fdist.N() + self.total_smooth;
+        return total_count
+
+    def initProb(self):
+        # Number of ngrams not in current LM
+        return {k:math.log((v+self.total_smooth)/float(self.total_count),2) for k,v in self.fdist.items()}
+
+    def get_log_prob(self, target_gram):
+        if target_gram in self.pdf:
+            return self.pdf[target_gram]
+        elif target_gram in self.vocab:
+            return self.gram_from_other_LM
+        else:
+            # gram does not match with vocab
+            return 0
+
+    def get_sentence_prob(self, target_grams):
+        result = self.lang_name, sum(self.get_log_prob(i) for i in target_grams)
+        return result
+
+
+
+''' Statistics '''
 
 def generate_single_ngram(n, sentence):
     """
@@ -21,51 +61,68 @@ def generate_ngram(n, sentences):
     #flatten the list
     return list(chain.from_iterable(result))
         
-def generate_fdist(ngrams):
-    return FreqDist(ngrams)
-
-def generate_MLEprob(ngrams):
-    return MLEProbDist(FreqDist(ngrams))
-
-def get_unique_list(ngrams):
-    return list(set(ngrams))
-
-def union_LM(langs):
+def generate_vocab(langs):
     """
     param langs: list of ngrams 
     return vocabulary
     """
     return set().union(*langs)
 
-def calcSmoothingConstant(ngrams, vocab):
-    smooth = len(set(vocab) - set(ngrams))
-    return smooth
-
-def calcProb(ngrams, vocab):
-    smooth = calcSmoothingConstant(ngrams, vocab)
-    fdist = generate_fdist(ngrams)
-    total_count = fdist.N() + smooth;
-    return {k:math.log(v+smooth/float(total_count)) for k,v in fdist.items()}
-    
-
-def read_input(filename):
-    lang_dict = {'malaysian': [], 'indonesian': [], 'tamil': [], 'other': []}
+''' Input/Output '''
+def read_train_input(filename):
+    lang_dict = {'malaysian': [], 'indonesian': [], 'tamil': []}
     with open(filename) as f:
         for line in f:
             sentence = line.split();
             label = sentence[0]
             content = sentence[1:]
-            lang_dict[label].append(' '.join(content))
+            if label in lang_dict:
+                lang_dict[label].append(' '.join(content))
     return lang_dict
 
+def read_test_input(filename):
+    test = []
+    with open(filename) as f:
+        for line in f:
+            test.append(line.strip(os.linesep)) 
+    return test
+
+def write_output(out_name, output):
+    with open(out_name, "w") as f:
+        f.write(output)
+
+''' Calculation '''
+
+def calcHighestProb(sentence, LModels):
+    target_grams = generate_single_ngram(4, sentence)
+    # Converts to dictionary 
+    prob_result = dict([lm.get_sentence_prob(target_grams) for k,lm in LModels.items()])
+    predictedLabel = max(prob_result, key=prob_result.get)
+    if(prob_result[predictedLabel] == 0):
+        predictedLabel = 'other'
+    return predictedLabel + " " + sentence
+
+def processQueries(query_list, LModels, out_name):
+    res = []
+    for num, sentence in enumerate(query_list):
+        predicted = calcHighestProb(sentence, LModels)
+        res.append(predicted)
+    write_output(out_name, "\n".join(res))
+
+
 def main():
-    lang_dict = read_input("input.correct.txt")
-    m_ngram = generate_ngram(4, lang_dict['malaysian'])
-    i_ngram = generate_ngram(4, lang_dict['indonesian'])
-    t_ngram = generate_ngram(4, lang_dict['tamil'])
-    vocab = union_LM([m_ngram, i_ngram, t_ngram])
-    m_prob = calcProb(m_ngram, vocab)
-    print(m_prob)
+    # reading input 
+    lang_dict = read_train_input("input.train.txt")
+    query_list = read_test_input("input.test.txt")
+
+    # generate ngrams
+    lang_ngram = {k: generate_ngram(4, v) for k,v in lang_dict.items()}
+
+    # generate total vocab of the three LMs
+    vocab = generate_vocab(lang_ngram.values())
+
+    LModels = {k: LangModel(k, v, vocab) for k,v in lang_ngram.items()}
+    prediction = processQueries(query_list, LModels, "output.txt")
 
 if __name__ == "__main__":
     main()
