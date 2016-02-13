ee5978640aeecc3f4547f638f4e196e061139423
jinifief <jinified@gmail.com>
2016-02-13 19:29:00 +0800

    Boiler for I/O

--- hw2/index.py
+++ hw2/index.py
@@ -1,1 +1,35 @@
 #!/use/bin/env python3
+import sys
+import getopt
+
+from nltk.tokenize import sent_tokenize
+
+''' Index files into dictornary and posting list
+Expected operations: tokenize, stemmer and case folding 
+'''
+
+def usage():
+    print "usage: " + sys.argv[0] + " -i training-input-file -d output-dictionary-file -p output-posting-file"
+
+input_file_i = dictionary_file_d = posting_file_p = None
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+except getopt.GetoptError, err:
+    usage()
+    sys.exit(2)
+for o, a in opts:
+    if o == '-i':
+        input_file_i = a
+    elif o == '-d':
+        dictionary_file_d = a
+    elif o == '-p':
+        posting_file_p = a
+    else:
+        assert False, "unhandled option"
+if input_file_i == None or dictionary_file_d == None or posting_file_p == None:
+    usage()
+    sys.exit(2)
+
+if __name__ == "__main__":
+    pass
+
