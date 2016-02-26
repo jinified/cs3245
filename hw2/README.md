02c9c063843d66a18354a6a8eb9dd50e9e4fea7f
jinifief <jinified@gmail.com>
2016-02-27 00:16:48 +0800

    Added test and utility methods

--- hw2/README.md
+++ hw2/README.md
@@ -1,3 +1,5 @@
 Resources:
 1. Reading text files as corpora
 http://stackoverflow.com/questions/4951751/creating-a-new-corpus-with-nltk
+2. Reading specific line from a file
+http://www.rosettacode.org/wiki/Read_a_specific_line_from_a_file#Python
--- hw2/test_util.py
+++ hw2/test_util.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+import os
 
 import util
 
+''' Test resources '''
+postings  = os.path.join(os.path.dirname(__file__), 'test/postings.txt')
+dictionary  = os.path.join(os.path.dirname(__file__), 'test/dictionary.txt')
+
 def test_get_posting_list():
-    pass
+    assert util.get_posting_list(1, postings) == ['1','2','3','4','5','6','6','7']
