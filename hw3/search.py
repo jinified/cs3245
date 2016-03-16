24048270487e818b819068367be499bac7e2bad7
jinifief <jinified@gmail.com>
2016-03-16 08:15:06 +0800

    Tested with queries1 from forum

--- hw3/search.py
+++ hw3/search.py
@@ -1,50 +1,49 @@
 #!/use/bin/env python3
 
 import sys
-import math
 import time
 import argparse
+import logging
 
 from util import *
 
 ''' Computes doc_ids that matches a free text query'''
 
-''' Globals '''
+# Setup environment
 start_time = time.time()
-dictionary = {}
-results = []
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 def search(queries_path, dictionary_path, posting_path, output_path, k):
+    dictionary = {}
+    results = []
+
     with open(dictionary_path) as dicts:
         for i, term in enumerate(dicts):
-            term, freq = term.strip('\r\n').strip('\n').split(' ')
+            term, freq = term.split(' ')
             dictionary[term] = (i + 1, freq)
 
     with open(queries_path) as queries:
-        # TODO refactor stripping
         for query in queries:
-            res = handleQuery(query.rstrip('\r\n').rstrip('\n').strip(' '), dictionary, posting_path, k)
-            rankedIds = rank_docIds(res)
-            results.append(' '.join(rankedIds))
-            print('========================')
-            print(query)
-            print(rankedIds)
-            print('========================')
+            result = get_score(handleQuery(query.rstrip('\r\n').rstrip('\n').strip(' '), 
+                               dictionary, posting_path, k))
+            results.append(' '.join(str(x) for x in result))
 
     with open(output_path, "w") as o:
         o.write('\n'.join(results))
         o.write('\n')
-    
+
+
 def handleQuery(query, dictionary, posting_path, k):
     ''' Returns (term, normalized tf-idf, posting_list) '''
     query = [normalize_token(i) for i in query.split(' ')]
     fdist = getFreqDist(query)
-    weights = [tf(freq) * float(dictionary.get(word, (0,0))[1]) for word, freq in fdist.items()]
+    weights = [tf(freq) * float(dictionary.get(word, (0, 0))[1]) for word, freq in fdist.items()]
     weights_norm = calcL2Norm(weights)
     # Prevent division by zero error
     return [(q, w/(weights_norm+0.000001), [] if q not in dictionary else get_posting_list(dictionary[q][0], posting_path))
         for q, w in zip(query, weights)]
-
 
 
 def parse_args(args=None):
--- hw3/util.py
+++ hw3/util.py
@@ -1,56 +1,50 @@
 #!/use/bin/env python3
 import os
-import glob
 import math
-import heapq
 from collections import defaultdict, OrderedDict
 from itertools import islice
 
 from nltk.probability import FreqDist
-from nltk.tokenize import sent_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
-from nltk.cluster.util import cosine_distance
 
-stemmer = PorterStemmer()   
-
-''' Queries '''
-def processQueries(queries, dictionary):
-    ''' Return normalized query and weight pair '''
-    queries = [normalize_token(i) for i in queries]
-    queries_tf = [tf(freq) for query in queries]
-    tf_norm = calcL2Norm(queries_tf)
-    return [(query, queries_tf/tf_norm) for query,query_tf in zip(queries, queries_tf)] 
+stemmer = PorterStemmer()
 
 ''' Statistics '''
 
-def rank_docIds(query_list, k=10):
-    # TODO Divide by document length for score '''
+
+def get_score(query_list, k=10):
     ranked = defaultdict(int)
-    
+
     # Ignore non-matched query
     for query in query_list:
-
         if query[1] > 0:
             for posting in query[2]:
                 ranked[int(posting[0])] += (query[1] * posting[1])
 
-    return sorted(ranked, key=ranked.get, reverse=True)[:k]
+    # Sort by id first to ensure those with same score will be ranked according to id
+    ranked = OrderedDict(sorted(ranked.items()))
+    return sorted(ranked, key=lambda x: (ranked.get(x), x), reverse=True)[:k]
+
 
 def getFreqDist(words):
     return FreqDist(words)
+
 
 def calcL2Norm(weights):
     ''' Calculates L2 Norm for given weights '''
     return math.sqrt(sum([weight**2 for weight in weights]))
 
+
 def tf_idf(N, term_freq, doc_freq):
-    return tf(term_freq) * df(doc_freq)
+    return tf(term_freq) * idf(doc_freq)
+
 
 def tf(term_freq):
     ''' Calculates term frequency with log base 10'''
     assert term_freq > 0
     return 1 + math.log10(term_freq)
+
 
 def idf(N, df):
     ''' Calculates inverse document frequency with log base 10'''
@@ -59,28 +53,34 @@ def idf(N, df):
 
 ''' Preprocess '''
 
+
 def normalize_token(token):
     # Converts unicode string to regular string
     return str(stemmer.stem(token.lower()))
 
 ''' Input/Output '''
 
+
 def get_doc_ids(filedir):
     return sorted(os.listdir(filedir), key=lambda x: int(x))
+
 
 def create_corpus(filedir):
     ''' Creates a corpus based on files that matches the regex in file directory'''
     return PlaintextCorpusReader(filedir, ".*")
 
+
 def list_to_string(target_list, delimiter=','):
     ''' Generates delimiter i.e ',' separated string'''
     return delimiter.join(target_list)
+
 
 def write_to_file(filepath, content):
     with open(filepath, 'w') as output:
         output.write(content)
 
 ''' Posting list '''
+
 
 def get_posting_list(index, filepath):
     '''Retrieves a posting list given a file handle
@@ -94,7 +94,7 @@ def get_posting_list(index, filepath):
             return [[float(j) for j in i.split(' ')] for i in posting_list]
         except StopIteration:
             print("Encounters end of iterator")
-        return posting_list 
-    
+        return posting_list
+
 if __name__ == "__main__":
     pass
